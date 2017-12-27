010335f5367e3ec970deda4c90f44725ba4e9fd0
Johan Andersson <repi@repi.se>
2017-12-27 18:07:41 +0800

    :sparkles: Download & extract shadertoy code

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,9 @@ extern crate clap;
 
 use clap::{Arg, App};
 use std::io::Write;
+use std::io::prelude::*;
 use std::fs::File;
-use std::path::PathBuf;
+use std::path::{Path,PathBuf};
 use std::error::Error;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use rayon::prelude::*;
@@ -19,7 +20,7 @@ fn convert_glsl_to_metal(name: &str, entry_point: &str, source: &str) -> Result<
     // convert to SPIR-V using shaderc
 
     let mut compiler = shaderc::Compiler::new().unwrap();
-    let mut options = shaderc::CompileOptions::new().unwrap();
+    let options = shaderc::CompileOptions::new().unwrap();
 
     let binary_result = compiler.compile_into_spirv(
         source,
@@ -55,6 +56,14 @@ fn convert_glsl_to_metal(name: &str, entry_point: &str, source: &str) -> Result<
     }
 }
 
+fn write_file(path: &Path, buf: &[u8]) {
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {:?}: {}", path, why.description()),
+        Ok(file) => file,
+    };
+
+    file.write_all(buf).unwrap();
+}
 
 fn main() {
     let matches = App::new("Shadertoy Downloader")
@@ -65,12 +74,20 @@ fn main() {
                          	.short("k")
                          	.long("apikey")
                          	.value_name("key")
+                            .required(true)
                          	.help("Set shadertoy API key to use. Create your key on https://www.shadertoy.com/myapps")
-                         .takes_value(true))
+                            .takes_value(true))
+                         .arg(Arg::with_name("search")
+                         	.short("s")
+                         	.long("search")
+                         	.value_name("stringy")
+                         	.help("Search string to filter which shadertoys to get")                         
+                            .takes_value(true))
                          .get_matches();
 
 
     let header_source = include_str!("shadertoy_header.glsl");
+/*
     let shadertoy_source = include_str!("shadertoy_test.glsl");
 
     let source = format!("{}{}", header_source, shadertoy_source);
@@ -78,20 +95,28 @@ fn main() {
     let metal_shader = convert_glsl_to_metal("source.glsl", "main", source.as_str()).unwrap();
 
     println!("{}", metal_shader);
-
-    return;
-
+*/
 
     let api_key = matches.value_of("apikey").unwrap();
 
     let mut shadertoys: Vec<String> = vec![];
     {
         let client = reqwest::Client::new();
-        let str = client.get(&format!("https://www.shadertoy.com/api/v1/shaders?key={}",api_key)).send().unwrap().text().unwrap();
 
-        let shaders_json = json::parse(&str).unwrap();
+        let query_str: String = {
+            if let Some(search_str) = matches.value_of("search") {
+                format!("https://www.shadertoy.com/api/v1/shaders/query/{}?key={}", search_str, api_key)
+            }
+            else {
+                format!("https://www.shadertoy.com/api/v1/shaders?key={}",api_key)
+            }
+        };
 
-        for v in shaders_json["Results"].members() {
+        let str = client.get(&query_str).send().unwrap().text().unwrap();
+
+        let json = json::parse(&str).unwrap();
+
+        for v in json["Results"].members() {
             if let Some(shadertoy) = v.as_str() {
                 shadertoys.push(String::from(shadertoy));
             }
@@ -107,39 +132,63 @@ fn main() {
         Ok(_) => {}
     }
 
-    let mut index = AtomicUsize::new(0);
+    let index = AtomicUsize::new(0);
 
     let client = reqwest::Client::new();
 
     //for shadertoy in shadertoys {
     shadertoys.par_iter().for_each(|shadertoy| {
         let path = PathBuf::from(format!("output/{}.json", shadertoy));
+
+        let mut json_str: String;
+
         if !path.exists() {
-            let str = client.get(&format!("https://www.shadertoy.com/api/v1/shaders/{}?key={}", shadertoy, api_key)).send().unwrap().text().unwrap();
+            json_str = client.get(&format!("https://www.shadertoy.com/api/v1/shaders/{}?key={}", shadertoy, api_key)).send().unwrap().text().unwrap();
 
             println!(
                 "shadertoy ({} / {}): {}, json size: {}",
                 index.load(Ordering::SeqCst),
                 shadertoys_len,
                 shadertoy,
-                str.len()
+                json_str.len()
             );
 
-            // Open a file in write-only mode, returns `io::Result<File>`
             let mut file = match File::create(&path) {
                 Err(why) => panic!("couldn't create {:?}: {}", path, why.description()),
                 Ok(file) => file,
             };
 
-            file.write_all(str.as_bytes());
-        } else {
+            file.write_all(json_str.as_bytes()).unwrap();
+        } 
+        else {
             println!(
-                "shadertoy ({} / {}): {}, already downloaded - skipping",
+                "shadertoy ({} / {}): {}, already downloaded",
                 index.load(Ordering::SeqCst),
                 shadertoys_len,
                 shadertoy
             );
+
+            let mut file = match File::open(&path) {
+                Err(why) => panic!("couldn't open {:?}: {}", path, why.description()),
+                Ok(file) => file,
+            };
+
+            json_str = String::new();
+            file.read_to_string(&mut json_str).unwrap();
         }
+
+
+        let json = json::parse(&json_str).unwrap();
+
+        for pass in json["Shader"]["renderpass"].members() {
+            if let Some(code) = pass["code"].as_str() {
+
+                let path = PathBuf::from(format!("output/{} {}.glsl", shadertoy, pass["name"].as_str().unwrap()));
+
+                write_file(&path, code.as_bytes());
+            }
+        }
+
         index.fetch_add(1, Ordering::SeqCst);
     });
 }
