961c23eaed6f18a2068f0d8084eb218df4e3bfc6
Johan Andersson <repi@repi.se>
2018-01-01 03:06:49 +0800

    :books: Additional README tweaks

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 //! Small [Shadertoy](http://shadertoy.com) browser & viewer for Mac built in [Rust](https://www.rust-lang.org).
 //! 
-//! This application uses the [Shadertoy API](http://shadertoy.com/api) to search for Shadertoys and then downloads them locally and converts them using [shaderc-rs](https://crates.io/crates/shaderc) and [spirv-cross](https://crates.io/crates/spirv_cross) to be natively rendered on Mac using [metal-rs](https://crates.io/crates/metal-rs).
+//! This application uses the [Shadertoy REST API](http://shadertoy.com/api) to search for Shadertoys and then downloads them locally and converts them using [`shaderc-rs`](https://crates.io/crates/shaderc) and [`spirv-cross`](https://crates.io/crates/spirv_cross) to be natively rendered on Mac using [`metal-rs`](https://crates.io/crates/metal-rs).
+//! 
+//! The API queries are done through the [`shadertoy`](https://crates.io/crates/shadertoy) crate, which can be found in  `src/shadertoy`
 
 #![allow(dead_code)]
 
