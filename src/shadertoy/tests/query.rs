ab132411aab2ed8671b6f8c37684935000d56972
Johan Andersson <repi@repi.se>
2017-12-31 16:24:02 +0800

    :umbrella: Add basic integration test for search API

---
+++ src/shadertoy/tests/query.rs
@@ -0,0 +1,13 @@
+extern crate shadertoy;
+
+#[test]
+fn test_query() {
+	let service = shadertoy::Service::new("Bd8tWD");
+
+	// try connecting to shadertoy.com and search for all "car" shadertoys	
+	// this of course requires one to be online
+	let result = service.search(Some("car"));	
+	
+	assert!(result.is_ok());
+	assert!(result.ok().unwrap().len() > 0);
+}
