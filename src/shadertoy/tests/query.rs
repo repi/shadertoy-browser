6568e39866d1f210674cf36a7b7a133be23839c4
Johan Andersson <repi@repi.se>
2018-01-01 02:21:37 +0800

    :green_heart: Fix rename in tests

--- src/shadertoy/tests/query.rs
+++ src/shadertoy/tests/query.rs
@@ -2,11 +2,11 @@ extern crate shadertoy;
 
 #[test]
 fn test_query() {
-    let service = shadertoy::Service::new("Bd8tWD");
+    let client = shadertoy::Client::new("Bd8tWD");
 
     // try connecting to shadertoy.com and search for all "car" shadertoys
     // this of course requires one to be online
-    let result = service.search(Some("car"));
+    let result = client.search(Some("car"));
 
     assert!(result.is_ok());
     assert!(result.ok().unwrap().len() > 0);
