1eb21280b3433d77a412986a39e60936bbfc48bc
Johan Andersson <repi@repi.se>
2018-01-02 01:39:21 +0800

    :umbrella: Add test case with invalid API key and with known shader

--- src/shadertoy/tests/query.rs
+++ src/shadertoy/tests/query.rs
@@ -18,8 +18,21 @@ fn test_query() {
     let shader = client.get_shader(&query[0]).unwrap();
     assert!(shader.renderpass.len() > 0);
 
+    // try getting a specific shader, this should suceed
+    let shader = client.get_shader("4d2BDy");
+    assert!(shader.is_ok());
 
     // try getting a random shader, this should fail
     let shader = client.get_shader("doesnt_exist");
     assert!(shader.is_err());
+}
+
+#[test]
+fn test_invalid_api_key() {
+    let client = shadertoy::Client::new("incorrect");
+
+    // try getting a specific shader, this should fail due to the API key
+    let shader = client.get_shader("4d2BDy");
+    assert!(shader.is_err());
+    println!("error: {}", shader.err().unwrap());
 }
